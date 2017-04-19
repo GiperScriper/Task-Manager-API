ea5d3242e9f167da0e198af0475e1b1b40814b65
Nikolay Martynov <tty0sh@gmail.com>
2017-04-19 22:13:32 +0300

    extend project model

--- src/models/project.js
+++ src/models/project.js
@@ -1,13 +1,29 @@
 const mongoose = require('mongoose');
 
-const Project = mongoose.model('Project', {
-  name: {
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  title: {
     type: String,
     required: true,
+    trim: true,
+    maxlength: 100,
+  },
+  description: {
+    type: String,
+    trim: true,
+    maxlength: 1000,
+  },
+  created: {
+    type: Date,
+    default: Date.now,
   },
   updated: {
-    type: Number,
+    type: Date,
+    default: Date.now,
   },
 });
+
+const Project = mongoose.model('Project', projectSchema);
 
 module.exports = { Project };
