abb1efc07d444cb700ac42e53b94e953ac60dfa3
Nikolay Martynov <tty0sh@gmail.com>
2017-05-14 17:28:53 +0300

    refactor authToken in async\await style

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,19 +1,18 @@
 const { User } = require('../models/user.model');
 
-function authToken(req, res, next) {
+async function authToken(req, res, next) {
   const token = req.headers['x-auth'];
-  User.findByToken(token)
-    .then((user) => {
-      if (!user) {
-        return Promise.reject();
-      }
-      req.user = user;
-      req.token = token;
-      next();
-    })
-    .catch((error) => {
-      res.status(401).json(error);
-    });
+  try {
+    const user = await User.findByToken(token);
+    if (!user) {
+      return Promise.reject();
+    }
+    req.user = user;
+    req.token = token;
+    next();
+  } catch (error) {
+    res.status(401).json(error);
+  }
 }
 
 module.exports = {
